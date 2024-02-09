a367bb6abfc05676439221955acc8f02685679c8
1234bhaskar <bhaskar2021chand@gmail.com>
2024-02-09 20:56:42 +0530

    PostList Comp Created

---
+++ React_Query_Concepts/src/Components/PostsList.jsx
@@ -0,0 +1,36 @@
+import {useQuery}  from '@tanstack/react-query'
+import React, { useEffect } from 'react'
+import fetchposts  from '../api/api.js';
+
+export default function PostsList() {
+const {data:postData,isError,isLoading,error} = useQuery({
+queryKey:["posts"],
+queryFn:fetchposts
+});
+    //console.log(data,isLoading,status);
+
+  return (
+    <div className='Container'>
+        {isLoading && <p>...Loading</p>}
+        {
+            isError && <p>{error?.message}</p>
+        }
+
+        {postData?.map((post)=>{
+                return(
+                    <div key={post.id} className='posts' >
+                        <div>{post.title}</div>
+                        {post.tags.map((tag)=>{
+                                <span key={tag}>{tag}</span>
+                            })
+                        }
+
+                    </div>
+
+                )
+            })
+        }
+        
+    </div>
+  )
+}

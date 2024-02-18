e03ee0cd2200ceeacf28eec36a55786b52315d20
1234bhaskar <bhaskar2021chand@gmail.com>
2024-02-18 18:12:21 +0530

    useMutation for Posting data

--- React_Query_Concepts/src/Components/PostsList.jsx
+++ React_Query_Concepts/src/Components/PostsList.jsx
@@ -1,6 +1,7 @@
-import {useQuery}  from '@tanstack/react-query'
+import {useMutation, useQuery}  from '@tanstack/react-query'
 import React, { useEffect } from 'react'
-import fetchposts  from '../api/api.js';
+import {fetchposts, addPost, fetchTags }  from '../api/api.js';
+
 
 export default function PostsList() {
 const {data:postData,isError,isLoading,error} = useQuery({
@@ -8,10 +9,57 @@ queryKey:["posts"],
 queryFn:fetchposts,
 staleTime:5000
 });
+
+//tags Fetch
+const {data:tagsData} = useQuery({
+queryKey:["tags"],
+queryFn:fetchTags,
+//staleTime:5000
+});
+
+
+//Post Data
+const {mutate,isError:isPostError,isPending,error:postError,reset}=useMutation({
+    mutationFn:addPost,
+    
+})
     //console.log(data,isLoading,status);
+
+    const handleSubmit=(e)=>{
+       e.preventDefault();
+        const formData =new FormData(e.target);
+        const title=formData.get("title");
+        const tags=Array.from(formData.keys()).filter((key)=>formData.get(key)==='on');
+
+       // console.log(title,tags);
+         if(!title ||!tags) return;
+
+         mutate({id:postData.length+1,title,tags})
+         e.target.reset();
+
+    }
 
   return (
     <div className='container'>
+
+        <form onSubmit={handleSubmit} >
+            <input type="text" placeholder='Enter your post..'
+            className='postBox'
+            name='title'/>
+            
+            <div className='tags'>
+             {
+                tagsData?.map((tag)=>{return(
+                    <span>
+                        <input type="checkbox" id={tag} name={tag} />
+                        <label htmlFor={tag}>{tag}</label>
+                    </span>
+                )})
+             }
+            </div>
+            <button >Post</button>
+        </form>
+
         {isLoading && <p>...Loading</p>}
         {
             isError && <p>{error?.message}</p>
--- React_Query_Concepts/src/api/api.js
+++ React_Query_Concepts/src/api/api.js
@@ -3,5 +3,20 @@ const fetchposts = async () => {
   const postData = await res.json();
   return postData;
 };
+const fetchTags = async () => {
+  const res = await fetch(`http://localhost:3000/tags`);
+  const tagsData = await res.json();
+  return tagsData;
+};
 
-export default fetchposts;
+const addPost = async (post) => {
+  const respoonse = await fetch("http://localhost:3000/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(post),
+  });
+  return Response.json();
+};
+export { fetchposts, addPost, fetchTags };
